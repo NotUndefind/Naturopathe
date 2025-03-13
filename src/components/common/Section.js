00eed28e87053bccb413f0cecd51369a78beeb8c
NotUndefind <julesbourin@icloud.com>
2025-03-13 15:20:38 +0100

    tailwind upgrade

--- src/components/common/Section.js
+++ src/components/common/Section.js
@@ -2,20 +2,20 @@ import React from "react";
 
 export default function Section() {
 	return (
-		<div className="flex flex-row flex-wrap place-content-center text-center content-center gap-5 ">
-			<div className="section">
+		<div className="flex flex-row flex-wrap justify-center align-middle gap-5">
+			<div className="section rounded-primary p-1 center border">
 				{/* eslint-disable-next-line */}
 				<a href="">A propos</a>
 			</div>
-			<div className="section">
+			<div className="section rounded-primary p-1 center border border-transparent">
 				{/* eslint-disable-next-line */}
 				<a href="">La Naturopathie</a>
 			</div>
-			<div className="section">
+			<div className="section rounded-primary p-1 center border ">
 				{/* eslint-disable-next-line */}
 				<a href="">Mon accompagnement</a>
 			</div>
-			<div className="section">
+			<div className="section rounded-primary p-1 center border ">
 				{/* eslint-disable-next-line */}
 				<a href="">Prestation/Tarifs</a>
 			</div>

5107d91034ce9408e36cdef3c905401a7dd40a2c
NotUndefind <julesbourin@icloud.com>
2025-05-15 19:29:05 +0200

    Automatic update

--- src/components/Contact/includes/ContactForm.js
+++ src/components/Contact/includes/ContactForm.js
@@ -3,9 +3,9 @@ import React from "react";
 
 export default function ContactForm() {
 	return (
-		<div className="grid gap-5 border-solid border-2 border-primary rounded-primary p-5 shadow-2xl flex-1 bg-[url('./assets/cab.jpeg')] bg-cover bg-no-repeat h-full content-start text-white min-h-[600px]">
-			<p>Tel : 06 09 89 45 10</p>
-			<p>Email : benedicte.manin.naturo@gmail.com</p>
+		<div className="grid gap-5 border-solid border-2 border-primary rounded-primary p-5 shadow-2xl flex-1 bg-[url('./assets/cab.jpeg')] bg-cover bg-no-repeat h-full content-start text-white min-h-[600px] text-center">
+			<p className="text-xl">Tel : 06 09 89 45 10</p>
+			<p className="text-xl">Email : benedicte.manin.naturo@gmail.com</p>
 		</div>
 	);
 }
